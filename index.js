e1eec97260b0a0fe78843b36f300af56c37c41fc
Emmanuel Anzele <eanzele@gmail.com>
2019-01-10 16:13:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,2 @@
 
 
-function iLoveTheBeatles(number) {
-  var holder = [];
-    do {
-      holder.push("I love the Beatles!");
-      number++;
-    }
-    while (number < 15);
-
-     return holder;
-}
